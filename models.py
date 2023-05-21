a8056211aa602aec7df31377c1c758ba09298849
Carlos-AMG <alberto-cmg@outlook.es>
2023-05-20 19:47:25 -0600

    Refactor models, Producto, Pedido, Cliente, Persona, Empleado done and some models are missing

--- models.py
+++ models.py
@@ -4,27 +4,40 @@ from database import Base
 
 class Empleado(Base):
     __tablename__ = "Empleado"
-    codigo = Column(Integer, primary_key=True, nullable=False)
-    num_empleado = Column(Integer, nullable=False)
+    RFC = Column(Integer, primary_key=True, nullabable=False)
+    personaID = Column(Integer, ForeignKey("Persona.id", ondelete="CASCADE"), nullable=False) #pendiente
     cargo = Column(String, nullable=False)
-    salario = Column(Float, server_default='TRUE', nullable=False)
-    RFC = Column(String, nullable=False)
-    fecha_inicio = Column(Date,nullable=False, default=func.current_date())
-
-
-class Cliente(Base):
-    __tablename__ = "Cliente"
-    num_cliente = Column(Integer, primary_key=True, nullable=False)
-    telefono = Column(String)
-
-class Producto(Base):
-    __tablename__ = "Producto"
-
-class Inventario(Base):
-    __tablename__ = "Inventario"
-
-class Pedido(Base):
-    __tablename__ = "Pedido"
+    salario = Column(Float, nullable=False)
+    fechaInicio = Column(Date, nullable=False, default=func.current_date())
 
 class Persona(Base):
     __tablename__ = "Persona"
+    ID = Column(Integer, ForeignKey=True, nullable=False)
+    nombre = Column(String, nullable=False)
+    apellidoP = Column(String, nullable=False)
+    apellidoM = Column(String, nullable=False)
+
+class Cliente(Base):
+    __tablename__ = "Cliente"
+    numCliente = Column(Integer, primary_key=True, nullable=False)
+    telefono = Column(String)
+    personaID = Column(Integer, nullable=False, )
+
+class Pedido(Base):
+    __tablename__ = "Pedido"
+    numPedido = Column(Integer, primary_key=True, nullable=False)
+    fechaPedido = Column(Date, nullable=False, default=func.current_date())
+    hora = Column(Date, nullable=False, default=func.current_hour())
+    cantidad = Column(Integer, nullable=False)
+    estado = Column(String, nullable=False)
+    total = Column(float,  nullable=False)
+    RFC_Empleado = Column(Integer, ForeignKey("Empleado.RFC", ondelete="CASCADE"), nullable=False)
+    numCliente = Column(Integer, ForeignKey("Cliente.numCliente", ondelete="CASCADE"), nullable=False)
+
+class Producto(Base):
+    __tablename__ = "Producto"
+    numProducto = Column(Integer, primary_key=True, nullable=False)
+    nombre = Column(String, nullable=False)
+    precio = Column(Float, nullable=False)
+    deescripcion = Column(String)
+
